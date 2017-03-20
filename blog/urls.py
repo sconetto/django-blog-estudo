94891fff6b2d2ac7078beaf736c3dddc91d482ce
João Pedro Sconetto <sconetto.joao@gmail.com>
2017-03-20 15:53:51 -0300

    Fixing the reverse match error and finishing the post form

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
-
-# {% url 'blog.views.post_detail' pk=post.pk %}
-# {% url 'blog.views.post_new' %}
